448a10a1e18c483dc9a938b09af5afae8b6237cc
thejsj <jorge.silva@thejsj.com>
2017-05-09 13:06:00 -0700

    Fix stuff

--- curl_server.bash
+++ curl_server.bash
@@ -6,10 +6,8 @@ retries=$max_retries
 
 
 while [ $retries -gt 0 ] ;  do
-    echo "Make request..."
-    curl -s -o /dev/null -w "%{http_code}" $host
-    readiness=$?
-    echo "DB Readiness: $readiness"
-    echo "remaining retries: ${retries}"
+    echo "Response: $(curl -s $host)"
+    echo "HTTP Response: $(curl -s -o /dev/null -w "%{http_code}" $host)"
+    echo "Remaining retries: ${retries}"
     sleep 1
 done
