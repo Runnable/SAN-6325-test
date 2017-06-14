35ad725a54f7675c7b139dd9e5caf9eb284130bd
thejsj <jorge.silva@thejsj.com>
2017-06-14 16:17:57 -0700

    Change aliases

--- curl_server.bash
+++ curl_server.bash
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-host=${host:-server}
+echo $HOST
 
 max_retries=${MAX_RETRIES:-100}
 retries=$max_retries
 
-
 while [ $retries -gt 0 ] ;  do
-    echo "Response: $(curl -s $host)"
-    echo "HTTP Response: $(curl -s -o /dev/null -w "%{http_code}" $host)"
+    echo "Response: $(curl -s $HOST)"
+    echo "HTTP Response: $(curl -s -o /dev/null -w "%{http_code}" $HOST)"
     echo "Remaining retries: ${retries}"
     sleep 1
 done
