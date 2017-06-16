28b5a0c683767f5bf226aaec67e8db2a4bdc040c
thejsj <jorge.silva@thejsj.com>
2017-06-16 11:25:21 -0700

    Add memcached

--- register_to_consul.bash
+++ register_to_consul.bash
@@ -1,3 +1,4 @@
 echo "Registering in consul"
 curl --request PUT --data '{ "Name": "memcached", "Address": "memcached", Port": 11211 }}' http://consul:8500/v1/agent/service/register
 echo "Starting memcached"
+memcached -u
