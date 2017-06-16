329c65c256a1088786105c4874a2ea836ec38570
thejsj <jorge.silva@thejsj.com>
2017-06-16 11:08:19 -0700

    Test consul

--- register_to_consul.bash
+++ register_to_consul.bash
@@ -1,3 +1,4 @@
-
-
-sleep 9999
+echo "Registering in consul"
+curl --request PUT --data '{ "Name": "memcached", "Address": "memcached", Port": 11211 }}' http://consul:8500/v1/agent/service/register
+echo "Starting memcached"
+memcahed
